0f6290bda6518825bd63b7ccad43b92a406dc18d
ccc <webdesign.up2cc3@gmail.com>
2017-04-24 22:09:48 +0200

    2 prueba

--- src/main/java/com/accent/ComponerPdf.java
+++ src/main/java/com/accent/ComponerPdf.java
@@ -36,34 +36,33 @@ public class ComponerPdf {
 
             for (int i = 0; i < 5; i++) {
                 Dept dpt = order.getDepts().get(i);
-                if ( dpt.items.size() > 0){
+                if (dpt.items.size() > 0) {
                     PDPage pag = new PDPage();
                     documentoFinal.addPage(pag);
-                }
 
 
-                int y = 700;
-                for (String l : dpt.items) {
-                    PDPageContentStream contentStream = new PDPageContentStream(documentoFinal, pag);
-                    contentStream.beginText();
-                    //Setting the font to the Content stream
-                    contentStream.setFont(PDType1Font.TIMES_ROMAN, 12);
+                    int y = 700;
+                    for (String l : dpt.items) {
+                        PDPageContentStream contentStream = new PDPageContentStream(documentoFinal, pag);
+                        contentStream.beginText();
+                        //Setting the font to the Content stream
+                        contentStream.setFont(PDType1Font.TIMES_ROMAN, 12);
 
-                    //Setting the position for the line
-                    contentStream.newLineAtOffset(25, y);
-                    y = y - 25;
+                        //Setting the position for the line
+                        contentStream.newLineAtOffset(25, y);
+                        y = y - 25;
 
-                    String text = l;
+                        String text = l;
 
-                    //Adding text in the form of string
-                    System.out.println(l);
-                    contentStream.showText(text);
-                    contentStream.endText();
+                        //Adding text in the form of string
+                        System.out.println(l);
+                        contentStream.showText(text);
+                        contentStream.endText();
 
-                    //Closing the content stream
-                    contentStream.close();
+                        //Closing the content stream
+                        contentStream.close();
 
-
+                    }
                 }
             }
             documentoFinal.save("C:\\Users\\ccc\\Documents\\accent-depts\\PDF Departments\\pdf.pdf");
